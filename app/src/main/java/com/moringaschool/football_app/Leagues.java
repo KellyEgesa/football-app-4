398ffc7d2590c4cd6f52ad3b6ee904c17e5f1c9b
KellyEgesa <kelly.egesa@gmail.com>
2021-01-11 01:33:17 +0300

    Added a toast on the main activity

--- app/src/main/java/com/moringaschool/football_app/Leagues.java
+++ app/src/main/java/com/moringaschool/football_app/Leagues.java
@@ -7,9 +7,9 @@ import android.widget.GridView;
 
 public class Leagues extends AppCompatActivity {
     GridView gridView;
-    String[] leagues = new String[]{"Premier League", "La Liga", "Seria A", "Bundesliga"};
-    String[] country = new String[]{"England", "Spain", "Italy", "Germany"};
-    String[] champions = new String[]{"Liverpool", "Real Madrid", "Juventus", "Bayern Munich"};
+    String[] leagues = new String[]{"Premier League", "La Liga", "Seria A", "Bundesliga", "Ligue A", "Major League Soccer"};
+    String[] country = new String[]{"England", "Spain", "Italy", "Germany", "France", "USA"};
+    String[] champions = new String[]{"Liverpool", "Real Madrid", "Juventus", "Bayern Munich", "Paris Saint German", "Atlanta United"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/moringaschool/football_app/MainActivity.java
+++ app/src/main/java/com/moringaschool/football_app/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +15,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.getStarted)
     Button mGetStartedButton;
+    @BindView(R.id.userName)
+    EditText mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == mGetStartedButton) {
             Intent intent = new Intent(MainActivity.this, Leagues.class);
             startActivity(intent);
+            String user = mUserName.getText().toString();
+            Toast.makeText(MainActivity.this, "Welcome: "+user, Toast.LENGTH_LONG).show();
         }
     }
 }
