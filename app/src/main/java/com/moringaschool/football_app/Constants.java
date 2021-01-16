14d0f57c244d601e76e9bada50b42781480d00e7
KellyEgesa <kelly.egesa@gmail.com>
2021-01-16 23:53:29 +0300

    Added constant class

---
+++ app/src/main/java/com/moringaschool/football_app/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.football_app;
+
+public class Constants {
+    public static final String FOOTBALL_BASE_URL = "http://api.football-data.org/v2/";
+    public static final String FOOTBALL_AUTH_TOKEN = BuildConfig.FOOTBALL_AUTH_TOKEN;
+}
---
+++ app/src/main/java/com/moringaschool/football_app/network/FootBallApi.java
@@ -0,0 +1,5 @@
+package com.moringaschool.football_app.network;
+
+public interface FootBallApi {
+
+}
