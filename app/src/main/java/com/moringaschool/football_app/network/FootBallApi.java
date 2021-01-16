3aa2638821d6df58d2ea1d671b3fcd62db4dc879
KellyEgesa <kelly.egesa@gmail.com>
2021-01-16 23:57:02 +0300

    Added FootBall Api interface for implementing retrofit

--- app/src/main/java/com/moringaschool/football_app/network/FootBallApi.java
+++ app/src/main/java/com/moringaschool/football_app/network/FootBallApi.java
@@ -1,5 +1,15 @@
 package com.moringaschool.football_app.network;
 
-public interface FootBallApi {
+import com.moringaschool.football_app.models.FootballDataCompetitionSearchResponse;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface FootBallApi {
+    @GET("competitions")
+    Call<FootballDataCompetitionSearchResponse> listCompetitions(
+            @Query("plan") String plan
+    );
 }
