0a78c4f57230910804360ca7a352f26fd43c4313
KellyEgesa <kelly.egesa@gmail.com>
2021-01-25 23:42:58 +0300

    Implemented button on LogIn Activity

--- app/src/main/java/com/moringaschool/football_app/ui/LogInActivity.java
+++ app/src/main/java/com/moringaschool/football_app/ui/LogInActivity.java
@@ -2,15 +2,48 @@ package com.moringaschool.football_app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moringaschool.football_app.R;
 
-public class LogInActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.logInTextEmail)
+    EditText mUserEmail;
+    @BindView(R.id.logInTextPassword)
+    EditText mUserPassword;
+    @BindView(R.id.logInButton)
+    Button mLogInButton;
+    @BindView(R.id.logInTextViewCreateAccount)
+    TextView mCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ButterKnife.bind(this);
+
+        mLogInButton.setOnClickListener(this);
+        mCreateAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLogInButton) {
+            Toast.makeText(LogInActivity.this, "Loggin in", Toast.LENGTH_LONG).show();
+        }
+        if (v == mCreateAccount) {
+            Intent intent = new Intent(LogInActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+        }
     }
 }
