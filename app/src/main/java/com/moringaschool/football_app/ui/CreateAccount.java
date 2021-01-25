d0448178da099fd7fe72f8acaa5c30ef5587671e
KellyEgesa <kelly.egesa@gmail.com>
2021-01-25 20:35:41 +0300

    Added create account activity

---
+++ app/src/main/java/com/moringaschool/football_app/ui/CreateAccount.java
@@ -0,0 +1,14 @@
+package com.moringaschool.football_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
